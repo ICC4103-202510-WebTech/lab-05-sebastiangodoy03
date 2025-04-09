48832cf1edcca786528c5215d7759f1b89756f9d
sebastiangodoy03 <sagodoy2@miuandes.cl>
2025-04-09 18:56:55 -0400

    not null

--- db/migrate/20250409192242_create_users.rb
+++ db/migrate/20250409192242_create_users.rb
@@ -1,10 +1,10 @@
 class CreateUsers < ActiveRecord::Migration[8.0]
   def change
     create_table :users do |t|
-      t.string :email
-      t.string :first_name
-      t.string :last_name
-      t.timestamps
+      t.string :email , null: false
+      t.string :first_name, null: false
+      t.string :last_name, null: false
+      t.timestamps, null: false
     end
   end
 end
--- db/migrate/20250409192948_create_chats.rb
+++ db/migrate/20250409192948_create_chats.rb
@@ -1,9 +1,9 @@
 class CreateChats < ActiveRecord::Migration[8.0]
   def change
     create_table :chats do |t|
-      t.integer :sender_id
-      t.integer :receiver_id
-      t.timestamps
+      t.integer :sender_id, null: false
+      t.integer :receiver_id, null: false
+      t.timestamps, null: false
     end
   end
 end
