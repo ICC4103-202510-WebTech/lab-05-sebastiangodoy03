d6dc4f519cc4454f533e2bad91858e81e3bf7335
sebastiangodoy03 <sagodoy2@miuandes.cl>
2025-04-16 16:25:50 -0400

    Lab 6

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :users, controller: 'users'
+  resources :chats, controller: 'chats'
+  resources :messages, controller: 'messages'
 end
