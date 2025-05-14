86d114d3b9287b894e995ed7075cf58b708201ce
sebastiangodoy03 <sagodoy2@miuandes.cl>
2025-05-14 15:48:27 -0400

    Edit the users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,7 +20,23 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to @user, notice: 'El usuario fue actualizado exitosamente.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
   params.require(:user).permit(:first_name, :last_name, :email)
   end
